0622ca408d985f685d6971d44877af77b7474625
ashalfarhan11 <ashal.farhan@qoala.id>
2021-03-28 13:27:35 +0700

    Use Chakra and custom theme

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from "~/layouts/Default.vue";
+import Chakra from "@chakra-ui/vue";
+import customTheme from "~/theme";
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.use(Chakra, {
+    extendTheme: customTheme,
+  });
+  Vue.component("Layout", DefaultLayout);
 }
