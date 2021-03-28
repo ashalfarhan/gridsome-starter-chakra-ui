0e38bcfb99ffa55fc9418aa65c59947d87163059
ashalfarhan11 <ashal.farhan@qoala.id>
2021-03-28 13:27:21 +0700

    Overriding App.vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <c-theme-provider>
+    <c-color-mode-provider>
+      <c-box font-family="body" as="main">
+        <c-reset />
+        <router-view />
+      </c-box>
+    </c-color-mode-provider>
+  </c-theme-provider>
+</template>
+
+<script>
+import {
+  CColorModeProvider,
+  CThemeProvider,
+  CReset,
+  CBox,
+} from "@chakra-ui/vue";
+export default {
+  name: "App",
+  components: {
+    CColorModeProvider,
+    CThemeProvider,
+    CReset,
+    CBox,
+  },
+};
+</script>
